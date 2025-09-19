02894ed6b5c73fab90374aca384d6769e22f45d4
Pedro Ruan <pedro.ruan251202@gmail.com>
2025-09-19 19:04:37 -0300

    starting second list

--- listas/semana4A_repetições_simples/problema1/dobra.c
+++ listas/semana4A_repetições_simples/problema1/dobra.c
@@ -13,14 +13,15 @@ int main () {
 
     //Validação para as dimnensões da folha serem maiores que o tamanho do bolso. 
     //Enquanto essa condição for verdadeira, o programa continuará validando os valores
-    while (comprimento_folha >= comprimento_bolso || largura_folha >= comprimento_bolso){
+    while (comprimento_folha >= comprimento_bolso && largura_folha >= comprimento_bolso){
         //Verificação para saber qual lado da folha é maior
         //O maior lado será dobrado
-        if (comprimento_folha >= comprimento_bolso){
-            comprimento_folha = comprimento_folha / 2;
+
+        if (comprimento_folha >= largura_folha) {
+            comprimento_folha /= 2;
         } else {
-            largura_folha = largura_folha / 2;
-        } 
+            largura_folha /= 2;
+        }
 
         //Contador que contabiliza o número de dobras necessárias para cada dobra
         contador++;
